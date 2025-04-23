2c1325d3856eea73e84304e60dbc6b0cfe99a9d6
mohfilms <mohfilms1@gmail.com>
2025-04-24 00:33:08 +0300

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white text-4xl">
+      موقع محمد سليمان الخولي قيد التطوير 🚀
+    </main>
+  );
+}
